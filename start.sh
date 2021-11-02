63b6695245ca26b81e0177cd93f4e7041da47900
Ubuntu <ubuntu@insidecode.me>
2021-11-02 05:00:47 +0000

    nginx https enablement

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 app="docker.insidecode"
 docker build -t ${app} .
-docker run -d -p http://www.insidecode.me -v nginx.conf:/etc/nginx/nginx.conf ${app}
+docker run -d -p 80:80 -v /home/ubuntu/techtok/nginx.conf:/etc/nginx/nginx.conf ${app}
