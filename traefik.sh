58ca3284cfc43f086062a2c1235374be70800e0b
Ubuntu <ubuntu@insidecode.me>
2021-11-07 13:29:12 +0000

    enabling traefik

---
+++ traefik.sh
@@ -0,0 +1,3 @@
+Docs: https://dev.to/tiangolo/deploying-fastapi-and-other-apps-with-https-powered-by-traefik-5dik
+docker-compose -f docker-compose.traefik.yml up
+docker-compose -f docker-compose.yml up -d
