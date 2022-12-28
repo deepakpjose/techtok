b9b0da6bc584d485741d6a00f32e300ec1d7dae3
Deepak Jose <deepakpjose@gmail.com>
2022-12-28 18:47:20 +0000

    enabling middleware

--- traefik.sh
+++ traefik.sh
@@ -1,3 +1,5 @@
 Docs: https://dev.to/tiangolo/deploying-fastapi-and-other-apps-with-https-powered-by-traefik-5dik
+Follow password setting session using openssl for setting up middleware.
+
 docker-compose -f docker-compose.traefik.yml up
 docker-compose -f docker-compose.yml up -d
